d328c0b4dddcab2f5eb0370e279b9ee436ea6029
leoBitto <leonardobitto@gmail.com>
2023-04-06 20:06:34 +0200

    added read me and create archive script

---
+++ archive_creator.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+sudo makedir ../archive
+sudo makedir ../archive/balancesheet
+sudo makedir ../archive/cash_flow
+sudo makedir ../archive/history
+sudo makedir ../archive/income_statement
